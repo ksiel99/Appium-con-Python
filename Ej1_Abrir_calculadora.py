2519a7622e1548f836c70e7c68ef9473da9d030a
Nicolas Alvarez <alvarez99@gmail.com>
2020-03-14 18:28:19 -0600

    Add files via upload

---
+++ Ej1_Abrir_calculadora.py
@@ -0,0 +1,10 @@
+from appium import webdriver
+
+driver ={
+  "platformName": "Android",
+  "platformVersion": "9",
+  "deviceName": "laurel_sprout",
+  "automationName": "UiAutomator1",
+  "appPackage": "com.google.android.calculator",
+  "appActivity": "com.android.calculator2.Calculator"
+}
